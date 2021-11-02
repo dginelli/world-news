72e7622aebf7c5f56b8c40c51fcd3e6696f90dcf
Davide Ginelli <ginellidavide@gmail.com>
2021-11-02 15:25:11 +0100

    Add examples of different implementations
    - Intent instead of Actions and Navigation Controller;
    - SplashScreen using SplashScreen API and drawable

--- app/src/main/java/it/unimib/worldnews/ui/LaunchScreenActivity.java
+++ app/src/main/java/it/unimib/worldnews/ui/LaunchScreenActivity.java
@@ -1,11 +1,12 @@
 package it.unimib.worldnews.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
+// import androidx.core.splashscreen.SplashScreen;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -14,28 +15,49 @@ import java.util.Set;
 import it.unimib.worldnews.R;
 import it.unimib.worldnews.utils.Constants;
 
+/**
+ * Launch (Splash) Screen Activity to manage the startup of
+ * the application and choose which Activity to start.
+ */
 public class LaunchScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Handle the splash screen transition.
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        /*
+         * Handle the splash screen transition with SplashScreen API
+         * (see https://developer.android.com/guide/topics/ui/splash-screen)
+         * SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        */
+
         setContentView(R.layout.activity_launch_screen);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.launch_screen_fragment_container_view);
         NavController navController = navHostFragment.getNavController();
 
+        // Start another Activity with Navigation Controller
         if (arePreferencesSet()) {
             navController.navigate(R.id.action_from_launchScreenActivity_to_newsActivity);
         } else {
             navController.navigate(R.id.action_from_launchScreenActivity_to_preferencesActivity);
         }
 
+        /* The equivalent way with explicit Intent
+        if (arePreferencesSet()) {
+            startActivity(new Intent(this, NewsActivity.class));
+        } else {
+            startActivity(new Intent(this, PreferencesActivity.class));
+        }*/
+
         finish();
     }
 
+    /**
+     * It checks if the preferences have already been set by the user.
+     *
+     * @return true if the preferences have been set, false otherwise.
+     */
     private boolean arePreferencesSet() {
         SharedPreferences sharedPref = getSharedPreferences(Constants.SHARED_PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
 
